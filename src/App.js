7aeadccf0786d9c982fb1d1a8e259bbd0df9a792
NickyEXE <dovern42@gmail.com>
2020-11-10 17:08:35 -0500

    adds welcome, changes app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './App.css';
 
 import TickerContainer from "./ticker/TickerContainer"
+import Welcome from "./ticker/Welcome"
 
-function App() {
-  return (
-    <div className="App">
-     <TickerContainer />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    ticker: true
+  }
+
+  toggleTicker = () => this.setState({ticker: !this.state.ticker})
+
+  render(){
+    return (
+      <div className="App">
+       {this.state.ticker ? <TickerContainer /> : <Welcome />}
+       <button onClick={this.toggleTicker}>Toggle Ticker!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
